64c82fc9f7a24f4dba930a472e931370a82312dc
noaova <noa_o2@walla.com>
2017-04-06 14:42:28 +0300

    zzzzzzzzzzzzzzz

---
+++ src/FinalProjectMAB/Controllers/MABController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FinalProjectMAB.Controllers
+{
+    public class MABController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        /* public string Welcome()
+         {
+             return "This is the Welcome action method...";
+         }
+         */
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
